36a080f03801cf8bec45a2bd49706f260d9afd16
Jonathan Warner <jewarner57@gmail.com>
2021-06-13 13:42:41 -0400

    feat: finish mood icons

--- src/Components/MoodForm/MoodForm.js
+++ src/Components/MoodForm/MoodForm.js
@@ -9,7 +9,9 @@ function MoodForm(props) {
       <img className="moodIcon" alt='happy' src={process.env.PUBLIC_URL + '/moods/happy.svg'} />
       <img className="moodIcon" alt='neutral' src={process.env.PUBLIC_URL + '/moods/neutral.svg'} />
       <img className="moodIcon" alt='sad' src={process.env.PUBLIC_URL + '/moods/sad.svg'} />
-      <img className="moodIcon" alt='happy' src={process.env.PUBLIC_URL + '/moods/happy.svg'} />
+      <img className="moodIcon" alt='silly' src={process.env.PUBLIC_URL + '/moods/silly.svg'} />
+      <img className="moodIcon" alt='chill' src={process.env.PUBLIC_URL + '/moods/chill.svg'} />
+      <img className="moodIcon" alt='angry' src={process.env.PUBLIC_URL + '/moods/angry.svg'} />
     </div>
   );
 }
